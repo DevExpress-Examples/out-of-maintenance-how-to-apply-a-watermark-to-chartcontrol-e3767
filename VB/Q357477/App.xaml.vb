3c6f2f45bfea864ee0dec2bda19790e7e1c78c59
DevExpressExampleBot <fakeEmail>
2022-12-18 15:23:08 +0400

    Source auto update [skip ci]

---
+++ VB/Q357477/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace Q357477
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
